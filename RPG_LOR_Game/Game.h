de102ef7919c99246769ff6e5bde07ae440bceec
serek90 <43785920+serek90@users.noreply.github.com>
2022-01-04 18:23:45 +0100

    Create working Makefile

--- RPG_LOR_Game/Game.h
+++ RPG_LOR_Game/Game.h
@@ -1,8 +1,8 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "SDL.h"
-#include "SDL_image.h"
+#include <SDL.h>
+#include <SDL_image.h>
 
 class Game
 {
--- RPG_LOR_Game/main.cpp
+++ RPG_LOR_Game/main.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "Game.h"
 
-int main(int argc, char * argv[])
+int main(int argc, char** argv)
 {
 	const uint32_t FPS = 60;
 	const uint32_t frameDelay = 1000 / 60; // ms between each frames
